2b6fd8b5edac35d2fc27b91a8ef3249823896ba3
KentonMcDaniel <kenton.mcdaniel@henryschein.com>
2018-07-11 15:47:45 -0600

    Initial Commit
    Added files for the test

---
+++ SubscriptionEncryption/Security.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionEncryption
+{
+    public class Security
+    {
+        public static string Encode(string clearText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Decode(string encodedText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SubscriptionEncryption/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionEncryption
+{
+    public class Subscription
+    {
+        public string Name { get; set; }
+        public bool IsSubscribed { get; set; }
+
+        public Subscription(string name, bool isSubscribed)
+        {
+            Name = name;
+            IsSubscribed = isSubscribed;
+        }
+    }
+}
